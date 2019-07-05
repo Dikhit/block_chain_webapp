2d0e54f1103bc60144cebaa221c0f9f3bb3e35e8
Dikhit <dikhitbhuyan@gmail.com>
2019-07-05 11:01:01 +0530

    service added and genesis block created

---
+++ src/app/pages/blockchain-viewer/blockchain-viewer.component.ts
@@ -0,0 +1,19 @@
+import { BlockchainService } from './../../service/blockchain.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blockchain-viewer',
+  templateUrl: './blockchain-viewer.component.html',
+  styleUrls: ['./blockchain-viewer.component.scss']
+})
+export class BlockchainViewerComponent implements OnInit {
+
+  public blocks = [];
+  constructor(private blockchainService : BlockchainService) {
+    this.blocks = blockchainService.getBlocks();
+  }
+
+  ngOnInit() {
+  }
+
+}
